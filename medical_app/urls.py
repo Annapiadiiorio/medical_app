e1c44b16050024ead62a87b8951539ed0111bf1e
Anna Pia Di Iorio <annapiadiiorio@df6961c2-20f8-46a6-9729-bbd9b7b70482.fritz.box>
2025-10-16 01:37:26 +0200

    Impostata homepage su dashboard

--- medical_app/urls.py
+++ medical_app/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from patients import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('patients.urls')),  
+path('admin/', admin.site.urls),
+    path('', views.stats, name='home'),  # 👈 questa riga fa puntare la home alla dashboard
+    path('dashboard/', views.stats, name='dashboard'),
+    path('add_patient/', views.add_patient, name='add_patient'),
+    path('add_exam/', views.add_exam, name='add_exam'),
+    path('stats/', views.stats, name='stats'),
 ]
