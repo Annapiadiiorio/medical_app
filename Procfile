532322a15880b96dd7a874ce4f3c4198979d9dad
Anna Pia Di Iorio <annapiadiiorio@MacBook-Air-di-Anna-Pia.local>
2025-10-16 16:15:35 +0200

    Run migrate/collectstatic before gunicorn on Render

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 release: python manage.py migrate
-web: gunicorn medical_app.wsgi
+web: python manage.py migrate --noinput && python manage.py collectstatic --noinput && gunicorn medical_app.wsgi:application --preload --timeout 120 --log-file -
+
 
--- medical_app/wsgi.py
+++ medical_app/wsgi.py
@@ -15,12 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical_app.settings')
 
 application = get_wsgi_application()
 # Esegui automaticamente le migrations su Render (solo se necessario)
-import os
-from django.core.management import call_command
-from django.db import OperationalError
-
-try:
-    call_command('migrate', interactive=False)
-except OperationalError:
-    print("⚠️  Database non pronto, skip migrations.")
 
